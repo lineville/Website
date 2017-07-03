de960a26f57d2eb31f919878e8ed2fb2d4f80847
Liam Neville <lineville@vassar.edu>
2017-07-03 16:52:15 -0700

    Styling updated with <hr> tags

--- MyWebSite/config/routes.rb
+++ MyWebSite/config/routes.rb
@@ -11,6 +11,7 @@ Rails.application.routes.draw do
 
   get 'resumes' => 'resumes#index'
 
-  get 'public/liam_resume.html' => 'resumes#liam_resume' 
+  get 'liam_resume.html' => 'resumes#liam_resume'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
